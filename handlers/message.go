3a7ee602acd57bef1807773d5ab74130914e0a53
Tronikel <contactdonatas@gmail.com>
2024-06-15 10:48:28 +0300

    read just body

--- handlers/message.go
+++ handlers/message.go
@@ -1,32 +1,35 @@
 package handlers
 
 import (
+	"io"
 	"tgator/middleware"
 
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/labstack/echo/v4"
 )
 
-type CreateMessageBind struct {
-	Raw string `json:"raw"`
-}
-
 func CreateMessage(c echo.Context) error {
 	cc := c.(*middleware.CustomContext)
 
-	bind := CreateMessageBind{}
-	if err := cc.Bind(&bind); err != nil {
+	body, err := io.ReadAll(c.Request().Body)
+	if err != nil {
+		return err
+	}
+
+	defer c.Request().Body.Close()
+
+	bodyStr := string(body)
+
+	if bodyStr == "" {
 		return echo.ErrBadRequest
 	}
 
-	if bind.Raw == "" {
-		return echo.ErrBadRequest
-	}
-
-	cc.Queries.CreateMessage(
+	if err := cc.Queries.CreateMessage(
 		cc.Request().Context(),
-		pgtype.Text{String: bind.Raw, Valid: true},
-	)
+		pgtype.Text{String: bodyStr, Valid: true},
+	); err != nil {
+		return echo.ErrBadGateway
+	}
 
 	return nil
 }
