d26907f4797d5ea97770cfd6b48d82819d66d2ca
Tronikel <contactdonatas@gmail.com>
2024-06-23 13:13:51 +0300

    init nil pointer fn

--- models/message.go
+++ models/message.go
@@ -6,8 +6,8 @@ type MessageModel struct {
 	ID        int64     `db:"id" goqu:"defaultifempty"`
 	CreatedAt time.Time `db:"created_at" goqu:"defaultifempty"`
 
-	Raw      string `db:"raw"`
-	SourceId int32  `db:"source_id"`
+	Raw      *string `db:"raw"`
+	SourceId int32   `db:"source_id"`
 
 	Source *SourceModel `embedded:""`
 }
